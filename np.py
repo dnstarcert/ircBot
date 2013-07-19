d8203a75b06802ac688a725f6846c5f2daae7f40
Dmitry Figol <sourg2@gmail.com>
2013-07-19 23:28:07 +0300

    refactor now playing function

--- np.py
+++ np.py
@@ -28,34 +28,26 @@ def command_np():
     audi = bus.get_object('org.mpris.audacious', '/Player')
     audm = audi.GetMetadata()
     if aud:
- 
         pos = aud.Position()
         playLength = aud.Length()
         length = aud.SongLength(pos)
-        tlength = (length > 0) and ("%d:%02d" % (length / 60, length % 60)) or "stream"
-	if tlength == 'stream':
-		np = "say np: " + " - 10[ " + unicode(audm['artist']).encode('utf-8') + " ]10" + " - " + unicode(audm['title']).encode('utf-8') 
-		return np
+        # tlength = (length > 0) and ("%d:%02d" % (length / 60, length % 60)) or "stream"
+        # if tlength == 'stream':
+        #     np = "say np: " + " - 10[ " + unicode(audm['artist']).encode('utf-8') + " ]10" + " - " + unicode(audm['title']).encode('utf-8') 
+        #     return np
         playSecs = aud.Time() / 1000
         n = playSecs * 11 / length
         info = aud.Info()
         try:
-         chars = chardet.detect(unicode(audm['artist']).encode('cp1251'))
+            chars = chardet.detect(unicode(audm['artist']).encode('cp1251'))
         except:
-         print "unknown"
-         return None
-	charsw = chars['encoding'].replace('windows-1251','utf8')
-	charsw = charsw.replace('utf-8','cp1251')
-	charsw = charsw.replace('utf8','utf-8')
-	charsw = charsw.replace('ascii','utf-8')
-	#print (unicode(audm).encode('utf-8'))
-	#print "Encoding : " + unicode(charsw).encode('utf-8')
-	#" - 10[ " + unicode(audm['genre']).encode(charsw) + " ]10" +
+            print "unknown"
+            return None
+        charsw = chars['encoding'].replace('windows-1251','utf8')
+        charsw = charsw.replace('utf-8','cp1251')
+        charsw = charsw.replace('utf8','utf-8')
+        charsw = charsw.replace('ascii','utf-8')
         np = "np: " + unicode(audm['artist']).encode(charsw) +  \
 " - " + unicode(audm['title']).encode(charsw) + " { " + ("%d:%02d" % (playSecs / 60, playSecs % 60)) + "/" + tlength + " }" +  " { " + str(info[0]/1000) +\
  " kbps" + " / " + str(info[1]/1000) + " kHz } "
     return np,charsw
- 
-
-
-    
